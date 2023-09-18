026781ad8411455589a08e389284a40356ba7f5b
sidharth <sidharthkumar@gmail.com>
2023-09-18 14:50:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Audi = new Car("Audi");
+
+        }
+
+    }
+
+    public sealed class Vechile
+    {
+        string vechile;
+        public void startEngine(string vechile)
+        {
+            this.vechile = vechile;
+            Console.WriteLine(vechile + " is start");
+        }
+        public void stopEngine()
+        {
+
+            Console.WriteLine(vechile + "is stop");
+        }
+    }
+
+    public class Car : Vechile
+    {
+        string name;
+        public Car(string name)
+        {
+            this.name = name;
+        }
+    }
+
+}
